8d0ef1fc52b21dd51448d24013f058aa99682ba4
lennardKiezewski <72935839+lennardKiezewski@users.noreply.github.com>
2021-09-23 00:08:27 +0200

    deletes js files no longer in use

--- public/functions/updateIngredientsTable.js
+++
@@ -1,6 +0,0 @@
-import { createTableContent } from "./createTableContent.js";
-export function updateIngredientsTable(tableContentElement, numberOfPersonsElement, amountPersons, ingredients) {
-    numberOfPersonsElement.innerHTML =
-        amountPersons > 1 ? amountPersons.toString() + " Personen" : "eine Person";
-    tableContentElement.innerHTML = createTableContent(ingredients, amountPersons);
-}
